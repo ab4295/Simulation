14e2159a22c91bbc27d3e4a714f522821ba99275
송창용 <sotagf31@gmail.com>
2020-02-28 14:36:43 +0900

    Initial Commit

---
+++ CG_Lab_Simul_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG_Lab_Simul_1
+//
+//  Created by 송창용 on 2020/02/28.
+//  Copyright © 2020 송창용. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
